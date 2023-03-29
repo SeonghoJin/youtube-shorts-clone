41fafd20102ed072e1acd0747e919aec7ed06688
seongho <seongho4996@naver.com>
2023-03-29 23:08:02 +0900

    feat: prototype 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import { youtubeUrlList } from './youtube';
+import './App.css';
+import { useEffect, useRef } from 'react';
+
+const YoutubeItem = ({ src }: { src: string }) => {
+  const ref = useRef<HTMLIFrameElement>(null);
+
+  return (
+    <div className="w-full h-[100vh]">
+      <div className="flex items-center justify-center">
+        <iframe
+          className="[scroll-snap-align:start]"
+          ref={ref}
+          width="315"
+          height="600"
+          src={src}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
+      </div>
+    </div>
+  );
+};
 
 function App() {
-  return <div className="App">test</div>;
+  const ref = useRef<HTMLUListElement>(null);
+  return (
+    <div className="w-full">
+      <div className="fixed left-[15px] top-[50%] flex flex-col gap-[10px]">
+        <button
+          className="bg-slate-500 p-[10px_12px]"
+          onClick={(e) => {
+            ref?.current?.scrollBy({
+              top: -1,
+              behavior: 'smooth',
+            });
+          }}
+        >
+          UP
+        </button>
+        <button
+          onClick={(e) => {
+            ref?.current?.scrollBy({
+              top: 1,
+              behavior: 'smooth',
+            });
+          }}
+          className="bg-orange-500 p-[10px_12px]"
+        >
+          DOWN
+        </button>
+      </div>
+      <div className="">
+        <ul
+          className="flex-col gap-[30px] m-[0_auto] overflow-y-scroll [scroll-snap-type:y_mandatory] h-[100vh]"
+          ref={ref}
+        >
+          {youtubeUrlList.map((url) => (
+            <li className=" justify-center items-center ">
+              <YoutubeItem src={url} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/youtube/index.ts
@@ -0,0 +1,17 @@
+export const youtubeUrlList = [
+    "https://www.youtube.com/embed/D7tv_QOlMBk",
+    "https://www.youtube.com/embed/yCzIEXBrFOs",
+    "https://www.youtube.com/embed/cjifVf9XKm0",
+    "https://www.youtube.com/embed/iSuZ61db9F8",
+    "https://www.youtube.com/embed/ZmUEw4lWRBk",
+    "https://www.youtube.com/embed/iSuZ61db9F8",
+    "https://www.youtube.com/embed/ZmUEw4lWRBk",
+    "https://www.youtube.com/embed/iSuZ61db9F8",
+    "https://www.youtube.com/embed/ZmUEw4lWRBk",
+    "https://www.youtube.com/embed/iSuZ61db9F8",
+    "https://www.youtube.com/embed/iSuZ61db9F8",
+    "https://www.youtube.com/embed/iSuZ61db9F8",
+    "https://www.youtube.com/embed/ZmUEw4lWRBk",
+    "https://www.youtube.com/embed/ZmUEw4lWRBk",
+    "https://www.youtube.com/embed/ZmUEw4lWRBk",
+];
